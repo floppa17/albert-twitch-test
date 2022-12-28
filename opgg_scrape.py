c00f8d58fab23ea3dbfbaa23d80d6f1ca4529deb
leon16c <121579173+leon16c@users.noreply.github.com>
2022-12-28 21:19:02 +0100

    Update opgg_scrape.py

--- opgg_scrape.py
+++ opgg_scrape.py
@@ -4,25 +4,29 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate
 import urllib.parse
  
-# VARIABLES
-name = "Main"
-summoner_name = "V9 Altbert"
-session = HTMLSession()
+def main():
+    result = opgg()
+    return result
 
-summoner_name_encoded = urllib.parse.quote(summoner_name)
-page = 'https://euw.op.gg/summoner/userName=' + summoner_name_encoded
-html = session.get(page)
-soup = BeautifulSoup(html.content, 'html.parser')
- 
-rank = soup.find("div", {"class": "tier"}).text.strip()
-lp = soup.find("div", {"class": "lp"}).text.strip()
-winrate = soup.find("div", {"class": "ratio"}).text.strip()
-wins, losses = soup.find("div", {"class": "win-lose"}).text.strip().split(" ")
-lastupdate = soup.find("div", {"class": "last-update"}).text.strip().partition("Last updated: ")[2]
+
+# VARIABLES
+
     
 
 
-def main():
-    return(""+summoner_name+" ist stuck in "+rank+", "+lp+", "+wins+" "+losses+", "+winrate+" ("+lastupdate+")")
+def opgg():
+    name = "Main"
+    summoner_name = "V9 Altbert"
+    session = HTMLSession()
 
-print(opgg())
+    summoner_name_encoded = urllib.parse.quote(summoner_name)
+    page = 'https://euw.op.gg/summoner/userName=' + summoner_name_encoded
+    html = session.get(page)
+    soup = BeautifulSoup(html.content, 'html.parser')
+ 
+    rank = soup.find("div", {"class": "tier"}).text.strip()
+    lp = soup.find("div", {"class": "lp"}).text.strip()
+    winrate = soup.find("div", {"class": "ratio"}).text.strip()
+    wins, losses = soup.find("div", {"class": "win-lose"}).text.strip().split(" ")
+    lastupdate = soup.find("div", {"class": "last-update"}).text.strip().partition("Last updated: ")[2]
+    return(""+summoner_name+" ist stuck in "+rank+", "+lp+", "+wins+" "+losses+", "+winrate+" ("+lastupdate+")")
